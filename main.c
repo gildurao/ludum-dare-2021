b55c15d95a881ed77dd821829c6d57ded0c53314
Gil Durão <43495596+gildurao@users.noreply.github.com>
2021-10-02 16:08:47 +0100

    add title screen

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
-
-
+#include <gb/gb.h>
 #include <stdio.h>
+#include "tiles/title_screen/letters.c"
+#include "maps/title_screen/title_map.c"
 
 void main(void)
 {
     puts("Hello World");
+}
+
+void showTitleScreen()
+{
+    set_bkg_data(0, 11, letters);
+    set_bkg_tiles(0, 0, titleMapWidth, titleMapHeight, titleMap);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    while (1)
+    {
+        scroll_bkg(1, 1);
+        delay(100);
+    }
 }
