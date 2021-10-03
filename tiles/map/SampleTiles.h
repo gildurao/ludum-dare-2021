b7efde255c4f805107ee8ffeaf56270cb9b2cbf6
freitzzz <1160907@isep.ipp.pt>
2021-10-03 21:48:47 +0100

    More enemies tiles for world map

--- tiles/map/SampleTiles.h
+++ tiles/map/SampleTiles.h
@@ -10,9 +10,9 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 15
+  Tiles                : 0 to 6
 
-  Palette colors       : None.
+  Palette colors       : Included.
   SGB Palette          : None.
   CGB Palette          : None.
 
@@ -25,6 +25,78 @@
 
 /* Bank of tiles. */
 #define tilesBank 0
+
+/* Super Gameboy palette 0 */
+#define tilesSGBPal0c0 6076
+#define tilesSGBPal0c1 8935
+#define tilesSGBPal0c2 6596
+#define tilesSGBPal0c3 5344
+
+/* Super Gameboy palette 1 */
+#define tilesSGBPal1c0 6076
+#define tilesSGBPal1c1 8935
+#define tilesSGBPal1c2 6596
+#define tilesSGBPal1c3 5344
+
+/* Super Gameboy palette 2 */
+#define tilesSGBPal2c0 6076
+#define tilesSGBPal2c1 8935
+#define tilesSGBPal2c2 6596
+#define tilesSGBPal2c3 5344
+
+/* Super Gameboy palette 3 */
+#define tilesSGBPal3c0 6076
+#define tilesSGBPal3c1 8935
+#define tilesSGBPal3c2 6596
+#define tilesSGBPal3c3 5344
+
+/* Gameboy Color palette 0 */
+#define tilesCGBPal0c0 6076
+#define tilesCGBPal0c1 8935
+#define tilesCGBPal0c2 6596
+#define tilesCGBPal0c3 5344
+
+/* Gameboy Color palette 1 */
+#define tilesCGBPal1c0 6076
+#define tilesCGBPal1c1 8935
+#define tilesCGBPal1c2 6596
+#define tilesCGBPal1c3 5344
+
+/* Gameboy Color palette 2 */
+#define tilesCGBPal2c0 6076
+#define tilesCGBPal2c1 8935
+#define tilesCGBPal2c2 6596
+#define tilesCGBPal2c3 5344
+
+/* Gameboy Color palette 3 */
+#define tilesCGBPal3c0 6076
+#define tilesCGBPal3c1 8935
+#define tilesCGBPal3c2 6596
+#define tilesCGBPal3c3 5344
+
+/* Gameboy Color palette 4 */
+#define tilesCGBPal4c0 6076
+#define tilesCGBPal4c1 8935
+#define tilesCGBPal4c2 6596
+#define tilesCGBPal4c3 5344
+
+/* Gameboy Color palette 5 */
+#define tilesCGBPal5c0 6076
+#define tilesCGBPal5c1 8935
+#define tilesCGBPal5c2 6596
+#define tilesCGBPal5c3 5344
+
+/* Gameboy Color palette 6 */
+#define tilesCGBPal6c0 6076
+#define tilesCGBPal6c1 8935
+#define tilesCGBPal6c2 6596
+#define tilesCGBPal6c3 5344
+
+/* Gameboy Color palette 7 */
+#define tilesCGBPal7c0 6076
+#define tilesCGBPal7c1 8935
+#define tilesCGBPal7c2 6596
+#define tilesCGBPal7c3 5344
 /* Start of tile array. */
 extern unsigned char tiles[];
 
