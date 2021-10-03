0d0b0b5e6c3301a36d6ab44c949f868483f546a9
João Ribeiro <1160906@isep.ipp.pt>
2021-10-03 18:54:50 +0100

    Physics 2.0

--- mecanics/physics.c
+++ mecanics/physics.c
@@ -1,10 +1,10 @@
 #include <gb/gb.h>
+#include <gb/drawing.h>
 #include <stdio.h>
 #include "../maps/SampleMap.h"
 
-extern fixed sposx, sposy;           /* Sprite position (fixed point) */
-extern fixed Last_sposx, Last_sposy; /* Last sprite position (fixed point) */
-extern fixed sspx, sspy;             /* Sprite speed (fixed point) */
+extern fixed sposx, sposy; /* Sprite position (fixed point) */
+extern int16_t sspx, sspy; /* Sprite speed (fixed point) */
 extern uint16_t camera_x, camera_y, old_camera_x, old_camera_y;
 void colision()
 {
@@ -18,19 +18,19 @@ void colision()
 
     if (sample_map[tile_index_under] == 0x00)
     {
-        sspy.w = 0;
+        sspy = 0;
     }
-    if (sample_map[tile_index_right] == 0x00)
-    {
-        sspx.w = 0;
-    }
-    if (sample_map[tile_index_right] == 0x00)
-    {
-        sspx.w = 0;
-    }
+    // if (sample_map[tile_index_right] == 0x00)
+    // {
+    //     sspx.w = 0;
+    // }
+    // if (sample_map[tile_index_right] == 0x00)
+    // {
+    //     sspx.w = 0;
+    // }
     if (sample_map[tile_index_under] == 0x02)
     {
-        sspy.w = sspy.w + 0x005;
+        sspy = sspy + 0x005;
     }
 }
 
@@ -68,9 +68,12 @@ uint8_t y_collision()
 
 uint8_t walls_right()
 {
-    uint16_t player_tile_x = ((sposx.b.h + 1) + camera_x) / 8;
-    uint16_t player_tile_y_low = (sposy.b.h - 9) / 8;
-    uint16_t player_tile_y_high = (sposy.b.h - 15) / 8;
+    uint16_t fx, fy;
+    fx = sposx.w + sspx;
+    fy = sposy.w + sspy;
+    uint16_t player_tile_x = (fx / 256 - 1 + camera_x) / 8;
+    uint16_t player_tile_y_low = (fy / 256 - 10) / 8;
+    uint16_t player_tile_y_high = (fy / 256 - 15) / 8;
     uint16_t tile_index_right_low = sample_mapWidth * (player_tile_y_low) + player_tile_x;
     uint16_t tile_index_right_high = sample_mapWidth * (player_tile_y_high) + player_tile_x;
     if (sample_map[tile_index_right_low] == 0x00 || sample_map[tile_index_right_high] == 0x00)
@@ -85,12 +88,78 @@ uint8_t walls_right()
 
 uint8_t walls_left()
 {
-    uint16_t player_tile_x = ((sposx.b.h - 9) + camera_x) / 8;
-    uint16_t player_tile_y_low = (sposy.b.h - 9) / 8;
-    uint16_t player_tile_y_high = (sposy.b.h - 15) / 8;
-    uint16_t tile_index_low = sample_mapWidth * (player_tile_y_low) + player_tile_x;
-    uint16_t tile_index_high = sample_mapWidth * (player_tile_y_high) + player_tile_x;
-    if (sample_map[tile_index_low] == 0x00 || sample_map[tile_index_high] == 0x00)
+
+    uint16_t fx, fy;
+    fx = sposx.w + sspx;
+    fy = sposy.w + sspy;
+    uint16_t player_tile_x = (fx / 256 + camera_x - 8) / 8;
+    uint16_t player_tile_y_low = (fy / 256 - 10) / 8;
+    uint16_t player_tile_y_high = (fy / 256 - 15) / 8;
+    uint16_t tile_index_right_low = sample_mapWidth * (player_tile_y_low) + player_tile_x;
+    uint16_t tile_index_right_high = sample_mapWidth * (player_tile_y_high) + player_tile_x;
+    if (sample_map[tile_index_right_low] == 0x00 || sample_map[tile_index_right_high] == 0x00)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+uint8_t walls_down()
+{
+
+    uint16_t fx, fy;
+    fx = sposx.w + sspx;
+    fy = sposy.w + sspy;
+    uint16_t player_tile_x_left = (fx / 256 - 6 + camera_x) / 8;
+    uint16_t player_tile_x_right = (fx / 256 - 2 + camera_x) / 8;
+    uint16_t player_tile_y = (fy / 256 - 9) / 8;
+    uint16_t tile_index_left = sample_mapWidth * (player_tile_y) + player_tile_x_left;
+    uint16_t tile_index_right = sample_mapWidth * (player_tile_y) + player_tile_x_right;
+
+    if (sample_map[tile_index_left] == 0x00 || sample_map[tile_index_right] == 0x00)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+uint8_t walls_down_check()
+{
+
+    uint16_t player_tile_x_left = (sposx.b.h - 6 + camera_x) / 8;
+    uint16_t player_tile_x_right = (sposx.b.h - 2 + camera_x) / 8;
+    uint16_t player_tile_y = (sposy.b.h - 8) / 8;
+    uint16_t tile_index_left = sample_mapWidth * (player_tile_y) + player_tile_x_left;
+    uint16_t tile_index_right = sample_mapWidth * (player_tile_y) + player_tile_x_right;
+
+    if (sample_map[tile_index_left] == 0x00 || sample_map[tile_index_right] == 0x00)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+uint8_t walls_up()
+{
+    uint16_t fx, fy;
+    fx = sposx.w + sspx;
+    fy = sposy.w + sspy;
+    uint16_t player_tile_x_left = (fx / 256 - 8 + camera_x) / 8;
+    uint16_t player_tile_x_right = (fx / 256 - 2 + camera_x) / 8;
+    uint16_t player_tile_y = (fy / 256 - 16) / 8;
+    uint16_t tile_index_left = sample_mapWidth * (player_tile_y) + player_tile_x_left;
+    uint16_t tile_index_right = sample_mapWidth * (player_tile_y) + player_tile_x_right;
+
+    if (sample_map[tile_index_left] == 0x00 || sample_map[tile_index_right] == 0x00)
     {
         return 1;
     }
